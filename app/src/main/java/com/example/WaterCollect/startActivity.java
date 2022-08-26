7520bcbfb11e0eb6aaa0511131786cc12cc03df8
eunjin1012 <koo5247@naver.com>
2022-08-27 01:02:58 +0900

    commit

---
+++ app/src/main/java/com/example/WaterCollect/startActivity.java
@@ -0,0 +1,19 @@
+package com.example.WaterCollect;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class startActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_main);
+    }
+
+}
+
+
+
+
+
