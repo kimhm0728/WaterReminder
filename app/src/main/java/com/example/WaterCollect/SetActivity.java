b468b58989a3103194046c5b3b6b0ca6bc090383
kimhm0728 <kimhm0728@naver.com>
2022-09-02 02:28:25 +0900

    style: SetActivity 디자인 수정

--- app/src/main/java/com/example/WaterCollect/SetActivity.java
+++ app/src/main/java/com/example/WaterCollect/SetActivity.java
@@ -1,72 +1,65 @@
 package com.example.WaterCollect;
 
 import android.app.AlarmManager;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
 
 
 public class SetActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    ToggleButton toggleButton;
-    Switch aSwitch;
+    private ImageView alarmImage;
+    private TextView alarmText;
+    private Switch alarmSwitch;
 
+    private PackageManager pm;
+    private ComponentName receiver;
+    private Intent alarmIntent;
+    private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
 
-        PackageManager pm = this.getPackageManager();
-        ComponentName receiver = new ComponentName(this, DeviceBootReceiver.class);
-        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        pm = this.getPackageManager();
+        receiver = new ComponentName(this, DeviceBootReceiver.class);
+        alarmIntent = new Intent(this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-//        imageView = new ImageView(this);
+        alarmImage = (ImageView) findViewById(R.id.imageView);
+        alarmText = (TextView) findViewById(R.id.alarmText);
+        alarmSwitch = (Switch) findViewById(R.id.alarmSwitch);
 
-        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
-        aSwitch = (Switch) findViewById(R.id.aSwitch);
-
-        //토글버튼 클릭
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        //스위치 클릭 이벤트
+        alarmSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
                 if (isChecked == true) {
-                    Toast.makeText(SetActivity.this, "토글클릭-ON", Toast.LENGTH_SHORT).show();
-                    //이미지를 교체
-                    imageView.setImageResource(R.drawable.on);
-                    //스위치를 on
-                    aSwitch.setChecked(true);
-
+                    Toast.makeText(SetActivity.this, "알림이 설정되었습니다.", Toast.LENGTH_SHORT).show();
+                    alarmImage.setImageResource(R.drawable.on);
+                    alarmText.setText("on");
 
                     // 현재 지정된 시간으로 알람 시간 설정
                     Calendar calendar = Calendar.getInstance();
                     calendar.setTimeInMillis(System.currentTimeMillis());
-                    calendar.set(Calendar.HOUR_OF_DAY, 20);
-                    calendar.set(Calendar.MINUTE, 21);
+                    calendar.set(Calendar.HOUR_OF_DAY, 18);
+                    calendar.set(Calendar.MINUTE, 0);
                     calendar.set(Calendar.SECOND, 0);
 
                     // 이미 지난 시간을 지정했다면 다음날 같은 시간으로 설정
@@ -74,45 +67,22 @@ public class SetActivity extends AppCompatActivity {
                         calendar.add(Calendar.DATE, 1);
                     }
 
-
                     diaryNotification(calendar);
 
                 } else {
-                    Toast.makeText(SetActivity.this, "토글클릭-OFF", Toast.LENGTH_SHORT).show();
-                    //이미지를 교체
-                    imageView.setImageResource(R.drawable.off);
-                    //스위치를 Off
-                    aSwitch.setChecked(false);
+                    Toast.makeText(SetActivity.this, "알림이 해제되었습니다.", Toast.LENGTH_SHORT).show();
 
+                    alarmImage.setImageResource(R.drawable.off);
+                    alarmText.setText("off");
+
+                    // 모든 알림 삭제
                     if (PendingIntent.getBroadcast(SetActivity.this, 0, alarmIntent, 0) != null && alarmManager != null) {
-                alarmManager.cancel(pendingIntent);
-                //Toast.makeText(this,"Notifications were disabled",Toast.LENGTH_SHORT).show();
-            }
-            pm.setComponentEnabledSetting(receiver,
-                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                    PackageManager.DONT_KILL_APP);
-                }
-
-            }
-
-        });
-
-        //스위치 클릭
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked == true) {
-                    Toast.makeText(SetActivity.this, "스위치-ON", Toast.LENGTH_SHORT).show();
-                    imageView.setImageResource(R.drawable.on);
-                    toggleButton.setChecked(true);
-
-                } else {
-                    Toast.makeText(SetActivity.this, "스위치-OFF", Toast.LENGTH_SHORT).show();
-                    imageView.setImageResource(R.drawable.off);
-                    toggleButton.setChecked(false);
-
+                        alarmManager.cancel(pendingIntent);
+                        //Toast.makeText(this,"Notifications were disabled",Toast.LENGTH_SHORT).show();
+                    }
+                    pm.setComponentEnabledSetting(receiver,
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP);
                 }
             }
         });
@@ -121,37 +91,22 @@ public class SetActivity extends AppCompatActivity {
 
     // 매일 특정 시간에 알림
     void diaryNotification(Calendar calendar) {
-        //PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-        //SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreference(this);
-        //Boolean dailyNotify = sharedPref.getBoolean(SetActivity.KEY_PREF_DAILY_NOTIFICATION, true);
 
+        if (alarmManager != null) {
 
-        PackageManager pm = this.getPackageManager();
-        ComponentName receiver = new ComponentName(this, DeviceBootReceiver.class);
-        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, pendingIntent);
 
-
-
-            if (alarmManager != null) {
-
-                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                        AlarmManager.INTERVAL_DAY, pendingIntent);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
             }
+        }
 
-            // 부팅 후 실행되는 리시버 사용가능하게 설정
-            pm.setComponentEnabledSetting(receiver,
-                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                    PackageManager.DONT_KILL_APP);
-
+        // 부팅 후 실행되는 리시버 사용가능하게 설정
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
 
     }
-
-
 
 }
