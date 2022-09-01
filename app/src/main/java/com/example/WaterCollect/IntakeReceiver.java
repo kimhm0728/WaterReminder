7822bd313cd70f08255665c4f55a7517c9fde12c
kimhm0728 <kimhm0728@naver.com>
2022-09-02 02:27:24 +0900

    feat: BroadcastReceiver 자정마다 실행

---
+++ app/src/main/java/com/example/WaterCollect/IntakeReceiver.java
@@ -0,0 +1,17 @@
+package com.example.WaterCollect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+public class IntakeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(@NonNull Context context, Intent intent) {
+        Log.e("test","test");
+        final MainActivity main = new MainActivity();
+        main.waterSum = 0;
+    }
+}
