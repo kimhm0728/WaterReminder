95a838a508535171f329f94253e5145d184cb5d1
eunjin1012 <koo5247@naver.com>
2022-09-02 19:09:02 +0900

    chore : numberpicker 및 아이콘 추가

--- app/src/main/java/com/example/WaterCollect/InputActivity.java
+++ app/src/main/java/com/example/WaterCollect/InputActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,28 +16,27 @@ import java.util.Locale;
 
 public class InputActivity extends AppCompatActivity {
 
-    private Button button;
-    private EditText edit;
     private TextView text;
     private String weight;
+    private NumberPicker number_picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        edit = findViewById(R.id.edit);
         text = findViewById(R.id.text);
         text.setText("");
+        number_picker=findViewById(R.id.number_picker);
+        number_picker.setMaxValue(200);
+        number_picker.setMinValue(0);
+        number_picker.setValue(0);
 
-        button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-
+        number_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onClick(View view) {
-                weight = edit.getText().toString();
-                String day = Integer.toString(Integer.parseInt(weight)*30);
+            public void onValueChange(NumberPicker picker, int oldValue, int newValue) {
+                String day = Integer.toString(newValue*30);
                 double amount = Double.parseDouble(day);
 
                 DecimalFormat formatter = new DecimalFormat("#,###");
