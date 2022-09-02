a5131940a98543efb5f205869ac3650900bb77ee
eunjin1012 <koo5247@naver.com>
2022-09-02 20:00:53 +0900

    chore : numberpicker, 아이콘 추가

--- app/src/main/java/com/example/WaterCollect/InputActivity.java
+++ app/src/main/java/com/example/WaterCollect/InputActivity.java
@@ -3,10 +3,8 @@ package com.example.WaterCollect;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,28 +13,28 @@ import java.util.Locale;
 
 public class InputActivity extends AppCompatActivity {
 
-    private Button button;
-    private EditText edit;
     private TextView text;
     private String weight;
+    private NumberPicker number_picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        edit = findViewById(R.id.edit);
         text = findViewById(R.id.text);
         text.setText("");
+        number_picker=findViewById(R.id.number_picker);
+        number_picker.setMaxValue(200);
+        number_picker.setMinValue(0);
+        number_picker.setValue(0);
 
-        button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-
+        number_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onClick(View view) {
-                weight = edit.getText().toString();
-                String day = Integer.toString(Integer.parseInt(weight)*30);
+            public void onValueChange(NumberPicker picker, int oldValue, int newValue) {
+                weight=Integer.toString(newValue);
+                String day = Integer.toString(newValue*30);
                 double amount = Double.parseDouble(day);
 
                 DecimalFormat formatter = new DecimalFormat("#,###");
