6dd185464bc2b13d798aaa15711e469a5548068e
kimhm0728 <kimhm0728@naver.com>
2022-09-02 16:40:01 +0900

    style: ui 수정

--- app/src/main/java/com/example/WaterCollect/InputActivity.java
+++ app/src/main/java/com/example/WaterCollect/InputActivity.java
@@ -3,10 +3,12 @@ package com.example.WaterCollect;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,14 +37,20 @@ public class InputActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                weight = edit.getText().toString();
-                String day = Integer.toString(Integer.parseInt(weight)*30);
-                double amount = Double.parseDouble(day);
+                if(!TextUtils.isEmpty(edit.getText())) {
+                    weight = edit.getText().toString();
+                    String day = Integer.toString(Integer.parseInt(weight)*30);
+                    double amount = Double.parseDouble(day);
 
-                DecimalFormat formatter = new DecimalFormat("#,###");
-                String formatted = formatter.format(amount);
+                    DecimalFormat formatter = new DecimalFormat("#,###");
+                    String formatted = formatter.format(amount);
 
-                text.setText(String.format(Locale.KOREA,"일일 섭취량은 %smL입니다.", formatted));
+                    text.setText(String.format(Locale.KOREA,"일일 섭취량은 %smL입니다.", formatted));
+                }
+                else {
+                    Toast.makeText(InputActivity.this, "몸무게가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
