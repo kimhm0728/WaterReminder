f8f5f973d94d547b59497882f443ba75d9cfa121
kimhm0728 <kimhm0728@naver.com>
2022-09-03 00:01:17 +0900

    chore: ui 수정

--- app/src/main/java/com/example/WaterCollect/InputActivity.java
+++ app/src/main/java/com/example/WaterCollect/InputActivity.java
@@ -3,19 +3,21 @@ package com.example.WaterCollect;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.text.DecimalFormat;
 import java.util.Locale;
 
 public class InputActivity extends AppCompatActivity {
 
     private TextView text;
     private String weight;
-    private NumberPicker number_picker;
+    private NumberPicker weight_picker;
+    private ImageButton xBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +25,31 @@ public class InputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_input);
 
         text = findViewById(R.id.text);
-        text.setText("");
-        number_picker=findViewById(R.id.number_picker);
-        number_picker.setMaxValue(200);
-        number_picker.setMinValue(0);
-        number_picker.setValue(0);
+        weight_picker = findViewById(R.id.weight);
+        xBtn = findViewById(R.id.x_btn);
+        weight_picker.setMaxValue(200);
+        weight_picker.setMinValue(0);
+        weight_picker.setValue(0);
 
-
-        number_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        weight_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldValue, int newValue) {
-                weight=Integer.toString(newValue);
-                String day = Integer.toString(newValue*30);
-                double amount = Double.parseDouble(day);
+                weight = Integer.toString(newValue);
+                String day = StringChanger.decimalComma(newValue*30);
 
-                DecimalFormat formatter = new DecimalFormat("#,###");
-                String formatted = formatter.format(amount);
-
-                text.setText(String.format(Locale.KOREA,"일일 섭취량은 %smL입니다.", formatted));
+                text.setText(String.format(Locale.KOREA,"하루 권장량은 %smL입니다.", day));
             }
         });
 
+        xBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("weight", weight);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 
     @Override
