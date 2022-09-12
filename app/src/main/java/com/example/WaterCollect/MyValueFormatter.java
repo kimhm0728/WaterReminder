eadef4d6f56fb9ada116d92ecf7f0d8ec0dcb9b6
kimhm0728 <kimhm0728@naver.com>
2022-09-13 04:07:57 +0900

    chore: 통계 페이지 ui 수정

---
+++ app/src/main/java/com/example/WaterCollect/MyValueFormatter.java
@@ -0,0 +1,21 @@
+package com.example.WaterCollect;
+
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+import java.text.DecimalFormat;
+
+// 소숫점 한 자리까지 보이기 위한 Formatter
+public class MyValueFormatter extends ValueFormatter implements IAxisValueFormatter {
+
+    private DecimalFormat mFormat;
+
+    public MyValueFormatter() {
+        mFormat = new DecimalFormat("#,###,###");
+    }
+
+    @Override
+    public String getFormattedValue(float value) {
+        return mFormat.format((int) value) + "mL";
+    }
+}
