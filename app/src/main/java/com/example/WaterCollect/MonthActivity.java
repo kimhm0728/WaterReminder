f65b31e078658232104fecd59678ce7248723184
kimhm0728 <kimhm0728@naver.com>
2022-09-15 00:19:56 +0900

    feat: 월 통계 기능 추가

--- app/src/main/java/com/example/WaterCollect/MonthActivity.java
+++ app/src/main/java/com/example/WaterCollect/MonthActivity.java
@@ -12,19 +12,29 @@ import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
 
+import java.util.Locale;
+
 public class MonthActivity extends Fragment {
 
-    private BarChart barchart;
+    private BarChart barChart;
     private TextView sum;
-    private TextView liter;
     private TextView average;
-    private TextView liter2;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.activity_month, container,false);
+        View v = inflater.inflate(R.layout.activity_month, container, false);
 
+        barChart = v.findViewById(R.id.chart);
+        sum = v.findViewById(R.id.sum_liter);
+        average = v.findViewById(R.id.average_liter);
+
+        ChartSetter chartSetter = new ChartSetter(12);
+        barChart = chartSetter.barChartSet(barChart);
+        sum.setText(String.format(Locale.KOREA, "%.3fL", (double)chartSetter.getSum()/1000));
+        average.setText(String.format(Locale.KOREA, "%.3fL", (double)chartSetter.getAvg()/1000));
+
+        barChart.invalidate(); // 차트 업데이트
         return v;
     }
 }
