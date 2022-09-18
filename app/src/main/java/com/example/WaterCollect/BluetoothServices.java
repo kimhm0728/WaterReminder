33301e05f7db8d63ba48fbc86ecf700c0e12978a
kimhm0728 <kimhm0728@naver.com>
2022-09-19 01:41:15 +0900

    chore: 블루투스 서비스 데이터 전송

--- app/src/main/java/com/example/WaterCollect/BluetoothServices.java
+++ app/src/main/java/com/example/WaterCollect/BluetoothServices.java
@@ -5,31 +5,28 @@ import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.ParcelUuid;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import java.util.Vector;
 
 public class BluetoothServices extends Service {
     private BluetoothAdapter mBluetoothAdapter;
-    public static final String B_DEVICE = "MY DEVICE";
-    public static final String B_UUID = "00001101-0000-1000-8000-00805f9b34fb";
-    // 00000000-0000-1000-8000-00805f9b34fb
 
     public static final int STATE_NONE = 0;
     public static final int STATE_LISTEN = 1;
@@ -38,33 +35,24 @@ public class BluetoothServices extends Service {
 
     private ConnectBtThread mConnectThread;
     private static ConnectedBtThread mConnectedThread;
+    private BluetoothDevice mBluetoothDevice; // 블루투스 디바이스
+    private Set<BluetoothDevice> devices; // 블루투스 디바이스 데이터 셋
 
     private static Handler mHandler = null;
     public static int mState = STATE_NONE;
-    public static String deviceName;
-    public static BluetoothDevice sDevice = null;
-    public Vector<Byte> packData = new Vector<>(2048);
 
-    //IBinder mIBinder = new LocalBinder();
-
-    private BluetoothDevice device;
-    private static final int REQUEST_ENABLE_BT = 10; // 블루투스 활성화 상태
-    private BluetoothAdapter bluetoothAdapter; // 블루투스 어댑터
-    private Set<BluetoothDevice> devices; // 블루투스 디바이스 데이터 셋
-    private BluetoothDevice bluetoothDevice; // 블루투스 디바이스
-    private OutputStream outputStream = null; //블루투스에 데이터를 출력하기 위한 출력 스트림
-    private InputStream inputStream = null; //블루투스에 데이터를 입력하기 위한 입력 스트림
-    private Thread workerThread = null; //문자열 수신에 사용되는 스레드
-    private byte[] readBuffer; //수신된 문자열 저장 버퍼
-    private int readBufferPosition; //버퍼  내 문자 저장 위치
-    private int pairedDeviceCount;  //페어링 된 기기의 크기를 지정할 변수
-    private List<String> list;
+    private Context context = this;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         //mHandler = getApplication().getHandler();
         return mBinder;
+    }
+
+    @Override
+    public void onCreate(){
+        Toast.makeText(this, "The new Service was Created", Toast.LENGTH_LONG).show();
     }
 
     private final IBinder mBinder = new LocalBinder();
@@ -79,19 +67,13 @@ public class BluetoothServices extends Service {
     @SuppressLint("MissingPermission")
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); //블루투스 어댑터를 디폴트 어댑터로 설정
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        devices = mBluetoothAdapter.getBondedDevices(); // 페어링 되어있는 블루투스 기기를 탐색
 
-//이미 페어링 되어있는 블루투스 기기를 탐색
-        devices = mBluetoothAdapter.getBondedDevices();
-        //페어링 된 디바이스 크기 저장
-        pairedDeviceCount = devices.size();
-        //페어링 된 장치가 없는 경우
+        String device = intent.getStringExtra("bluetooth_device");
+        connectToDevice(device);
 
-        String deviceg = intent.getStringExtra("bluetooth_device");
-
-        connectToDevice(deviceg);
-
-        return START_STICKY;
+        return START_REDELIVER_INTENT;
     }
 
     @SuppressLint("MissingPermission")
@@ -100,24 +82,22 @@ public class BluetoothServices extends Service {
         for (BluetoothDevice tempDevice : devices) {
             //사용자가 선택한 이름과 같은 디바이스로 설정하고 반복문 종료
             if (macAddress.equals(tempDevice.getName())) {
-                bluetoothDevice = tempDevice;
+                mBluetoothDevice = tempDevice;
                 break;
             }
-
         }
 
-        //device = mBluetoothAdapter.getRemoteDevice(macAddress);
-        if (mState == STATE_CONNECTING){
-            if (mConnectThread != null){
+        if (mState == STATE_CONNECTING) {
+            if (mConnectThread != null) {
                 mConnectThread.cancel();
                 mConnectThread = null;
             }
         }
-        if (mConnectedThread != null){
+        if (mConnectedThread != null) {
             mConnectedThread.cancel();
             mConnectedThread = null;
         }
-        mConnectThread = new ConnectBtThread(bluetoothDevice);
+        mConnectThread = new ConnectBtThread(mBluetoothDevice);
         mConnectThread.start();
         setState(STATE_CONNECTING);
     }
@@ -147,25 +127,17 @@ public class BluetoothServices extends Service {
         stopSelf();
     }
 
-    public void sendData(String message){
-        if (mConnectedThread!= null){
-            mConnectedThread.write(message.getBytes());
-        }else {
-            Toast.makeText(BluetoothServices.this,"Failed to send data",Toast.LENGTH_SHORT).show();
-        }
-    }
-
     @SuppressLint("MissingPermission")
     @Override
     public boolean stopService(Intent name) {
         setState(STATE_NONE);
 
-        if (mConnectThread != null){
+        if (mConnectThread != null) {
             mConnectThread.cancel();
             mConnectThread = null;
         }
 
-        if (mConnectedThread != null){
+        if (mConnectedThread != null) {
             mConnectedThread.cancel();
             mConnectedThread = null;
         }
@@ -174,45 +146,21 @@ public class BluetoothServices extends Service {
         return super.stopService(name);
     }
 
-    /*private synchronized void connected(BluetoothSocket mmSocket){
-
-        if (mConnectThread != null){
-            mConnectThread.cancel();
-            mConnectThread = null;
-        }
-        if (mConnectedThread != null){
-            mConnectedThread.cancel();
-            mConnectedThread = null;
-        }
-
-        mConnectedThread = new ConnectedBtThread(mmSocket);
-        mConnectedThread.start();
-
-
-        setState(STATE_CONNECTED);
-    }*/
-
-    private class ConnectBtThread extends Thread{
+    private class ConnectBtThread extends Thread {
         private final BluetoothSocket mSocket;
         private final BluetoothDevice mDevice;
-        //UUID생성
         private final ParcelUuid list[];
         private final UUID uuid;
-        //Rfcomm 채널을 통해 블루투스 디바이스와 통신하는 소켓 생성
 
         @SuppressLint("MissingPermission")
         public ConnectBtThread(@NonNull BluetoothDevice device) {
             list = device.getUuids();
             uuid = UUID.fromString(list[0].toString());
             mDevice = device;
-            BluetoothSocket bluetoothSocket = null; //블루투스 소켓
+            BluetoothSocket bluetoothSocket = null;
             try {
+                //Rfcomm 채널을 통해 블루투스 디바이스와 통신하는 소켓 생성
                 bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid);
-
-                outputStream = bluetoothSocket.getOutputStream();
-                inputStream = bluetoothSocket.getInputStream();
-                //receiveData();
-                Log.e("connect", "done");
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -229,24 +177,23 @@ public class BluetoothServices extends Service {
                 mSocket.connect();
                 Log.d("service","connect thread run method (connected)");
                 SharedPreferences pre = getSharedPreferences("BT_NAME",0);
-                pre.edit().putString("bluetooth_connected",mDevice.getName()).apply();
+                pre.edit().putString("bluetooth_connected", mDevice.getName()).commit();
 
             } catch (IOException e) {
-
                 try {
                     mSocket.close();
-                    Log.d("service","connect thread run method ( close function)");
+                    Log.d("service","connect thread run method (close function)");
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
                 e.printStackTrace();
             }
-            //connected(mSocket);
+
             mConnectedThread = new ConnectedBtThread(mSocket);
             mConnectedThread.start();
         }
 
-        public void cancel(){
+        public void cancel() {
             try {
                 mSocket.close();
                 Log.d("service","connect thread cancel method");
@@ -256,55 +203,76 @@ public class BluetoothServices extends Service {
         }
     }
 
-    private class ConnectedBtThread extends Thread{
+    private class ConnectedBtThread extends Thread {
         private final BluetoothSocket cSocket;
         private final InputStream inS;
-        private final OutputStream outS;
 
-        private byte[] buffer;
+        private byte[] readBuffer = new byte[1024]; // 데이터 수신을 위한 버퍼 생성
+        private int readBufferPosition = 0; // 버퍼 내 문자 저장 위치
+        final Handler handler = new Handler(Looper.getMainLooper());
+        final DataInserter[] task = new DataInserter[1];
+        String water;
 
-        public ConnectedBtThread(BluetoothSocket socket){
+        public ConnectedBtThread(BluetoothSocket socket) {
             cSocket = socket;
             InputStream tmpIn = null;
-            OutputStream tmpOut = null;
 
             try {
                 tmpIn = socket.getInputStream();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                tmpOut = socket.getOutputStream();
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
             inS = tmpIn;
-            outS = tmpOut;
         }
 
         @Override
         public void run() {
-            buffer = new byte[1024];
-            int mByte;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    int byteAvailable = inS.available();
+                    if (byteAvailable > 0) {
+                        //입력 스트림에서 바이트 단위로 읽어옴
+                        byte[] bytes = new byte[byteAvailable];
+                        inS.read(bytes);
+                        //입력 스트림 바이트를 한 바이트씩 읽어옴
+                        for (int i = 0; i < byteAvailable; i++) {
+                            byte tempByte = bytes[i];
+                            //개행문자를 기준으로 받음
+                            if (tempByte == '\n') {
+                                //readBuffer 배열을 encodeBytes로 복사
+                                byte[] encodedBytes = new byte[readBufferPosition];
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                //인코딩 된 바이트 배열을 문자열로 변환
+                                water = new String(encodedBytes, "UTF-8");
+                                readBufferPosition = 0;
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        water = water.replace("\r", "");
+                                        LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent("RECEIVED_DATA"));
+                                        // 받은 센서 값을 서버에 전송
+                                        task[0] = new DataInserter();
+                                        task[0].execute("http://" + MainActivity.getIpAddress() + "/insert.php", MainActivity.getDeviceNumber(), water, "send");
+                                    }
+                                });
+                            } // 개행문자가 아닐경우
+                            else {
+                                readBuffer[readBufferPosition++] = tempByte;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             try {
-                mByte= inS.read(buffer);
-            } catch (IOException e) {
+                //10초 마다 받아옴
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             Log.d("service","connected thread run method");
-
-        }
-
-
-        public void write(byte[] buff){
-            try {
-                outS.write(buff);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
         private void cancel(){
@@ -319,7 +287,6 @@ public class BluetoothServices extends Service {
 
     @Override
     public void onDestroy() {
-        this.stop();
-        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
     }
 }
